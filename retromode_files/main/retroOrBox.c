07455c188b6f069f7b2a98c24b4bbd6ca6d94381
Kjetil Hvalstrand <kjetil@teknobingo.no>
2017-10-14 15:01:44 +0200

    Implemented normal,or,xor draw box
    Implemented normal,or,xor draw box

--- retromode_files/main/retroOrBox.c
+++ retromode_files/main/retroOrBox.c
@@ -58,12 +58,50 @@
 
 void _retromode_retroOrBox(struct RetroModeIFace *Self,
        struct retroScreen * screen,
-       int x0,
-       int y0,
        int x1,
        int y1,
+       int x2,
+       int y2,
        unsigned char or_mask)
 {
+	int ox1 = x1;
+	int oy1 = y1;
+	int ox2 = x2;
+	int oy2 = y2;
+	int x,y;
+	unsigned char *memory;
 
+	if (x1<0) x1 = 0;
+	if (x2>screen->width-1) x2 = screen -> width-1;
+	if (y1<0) y1 = 0;
+	if (y2>screen->height-1) y2 = screen -> height-1;
+
+	// draw top 
+	if (oy1>-1)
+	{
+		memory = screen -> Memory + (screen -> width * y1) + x1;
+		for ( x=x1; x<=x2; x++) { *memory++ |= or_mask; }
+	}
+
+	// draw vertical left
+	if (ox1>-1)
+	{
+		memory = screen -> Memory + (screen -> width * (y1+1)) + x1;
+		for ( y=y1+1; y<y2; y++) { *memory |= or_mask; memory += screen->width; }
+	}
+
+	// draw bottom
+	if (oy2<screen->height)
+	{
+		memory = screen -> Memory + (screen -> width * y2) + x1;
+		for ( x=x1; x<=x2; x++) { *memory++ |= or_mask; }
+	}
+
+	// draw vertical right	
+	if (ox2<screen->width)
+	{
+		memory = screen -> Memory + (screen -> width * (y1+1)) + x2;
+		for ( y=y1+1; y<y2; y++) { *memory |= or_mask; memory += screen->width; }
+	}
 }
 
--- retromode_files/main/retroXorBox.c
+++ retromode_files/main/retroXorBox.c
@@ -58,11 +58,50 @@
 
 void _retromode_retroXorBox(struct retromodeIFace *Self,
        struct retroScreen * screen,
-       int x0,
-       int y0,
        int x1,
        int y1,
+       int x2,
+       int y2,
        unsigned char xor_mask)
 {
+	int ox1 = x1;
+	int oy1 = y1;
+	int ox2 = x2;
+	int oy2 = y2;
+	int x,y;
+	unsigned char *memory;
+
+	if (x1<0) x1 = 0;
+	if (x2>screen->width-1) x2 = screen -> width-1;
+	if (y1<0) y1 = 0;
+	if (y2>screen->height-1) y2 = screen -> height-1;
+
+	// draw top 
+	if (oy1>-1)
+	{
+		memory = screen -> Memory + (screen -> width * y1) + x1;
+		for ( x=x1; x<=x2; x++) { *memory++ ^= xor_mask; }
+	}
+
+	// draw vertical left
+	if (ox1>-1)
+	{
+		memory = screen -> Memory + (screen -> width * (y1+1)) + x1;
+		for ( y=y1+1; y<y2; y++) { *memory ^= xor_mask; memory += screen->width; }
+	}
+
+	// draw bottom
+	if (oy2<screen->height)
+	{
+		memory = screen -> Memory + (screen -> width * y2) + x1;
+		for ( x=x1; x<=x2; x++) { *memory++ ^= xor_mask; }
+	}
+
+	// draw vertical right	
+	if (ox2<screen->width)
+	{
+		memory = screen -> Memory + (screen -> width * (y1+1)) + x2;
+		for ( y=y1+1; y<y2; y++) { *memory ^= xor_mask; memory += screen->width; }
+	}
 }
 
