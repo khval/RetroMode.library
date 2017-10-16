880917e6ec10465c40bf97bb927d3f9bf936b792
Kjetil Hvalstrand <kjetil@teknobingo.no>
2017-10-16 23:04:06 +0200

    Renamed functions
    Renamed functions

---
+++ retromode_files/main/retroClearVideo.c
@@ -0,0 +1,72 @@
+/* :ts=4
+ *  $VER: retroClearVideo.c $Revision$ (16-Oct-2017)
+ *
+ *  This file is part of retromode.
+ *
+ *  Copyright (c) 2017 LiveForIt Software.
+ *  MIT License
+ *
+ * $Id$
+ *
+ * $Log$
+ *
+ *
+ */
+
+
+#include <exec/exec.h>
+#include <proto/exec.h>
+#include <dos/dos.h>
+#include <exec/types.h>
+#include <libraries/retromode.h>
+#include <proto/retromode.h>
+#include <stdarg.h>
+
+/****** retromode/main/retroClearVideo ******************************************
+*
+*   NAME
+*      retroClearVideo -- Description
+*
+*   SYNOPSIS
+*      void retroClearVideo(struct retroVideo * video);
+*
+*   FUNCTION
+*
+*   INPUTS
+*       video - 
+*
+*   RESULT
+*       This function does not return a result
+*
+*   EXAMPLE
+*
+*   NOTES
+*
+*   BUGS
+*
+*   SEE ALSO
+*
+*****************************************************************************
+*
+*/
+
+void _retromode_retroClearVideo(struct RetroModeIFace *Self,
+       struct retroVideo * video)
+{
+	int size =  video->BytesPerRow * video -> height ;
+	unsigned int *mem;
+
+	size /= 4; // get number of ints.
+	size /= 4; // unroll 4.
+
+	mem = video -> Memory;
+
+	while (size-- )
+	{
+		*mem++=0;
+		*mem++=0;
+		*mem++=0;
+		*mem++=0;
+	}
+}
+
---
+++ retromode_files/main/retroDmaVideo.c
@@ -0,0 +1,65 @@
+/* :ts=4
+ *  $VER: retroDmaVideo.c $Revision$ (16-Oct-2017)
+ *
+ *  This file is part of retromode.
+ *
+ *  Copyright (c) 2017 LiveForIt Software.
+ *  MIT License
+ *
+ * $Id$
+ *
+ * $Log$
+ *
+ *
+ */
+
+
+#include <exec/exec.h>
+#include <proto/exec.h>
+#include <dos/dos.h>
+#include <exec/types.h>
+#include <libraries/retromode.h>
+#include <proto/retromode.h>
+#include <stdarg.h>
+
+#include "libbase.h"
+
+/****** retromode/main/retroDmaVideo ******************************************
+*
+*   NAME
+*      retroDmaVideo -- Description
+*
+*   SYNOPSIS
+*      void retroDmaVideo(struct retroVideo * video);
+*
+*   FUNCTION
+*
+*   INPUTS
+*       video - 
+*
+*   RESULT
+*       This function does not return a result
+*
+*   EXAMPLE
+*
+*   NOTES
+*
+*   BUGS
+*
+*   SEE ALSO
+*
+*****************************************************************************
+*
+*/
+
+void _retromode_retroDmaVideo(struct RetroModeIFace *Self,
+       struct retroVideo * video)
+{
+	struct RetroLibrary *libBase = (struct RetroLibrary *) Self -> Data.LibBase;
+
+	// move gfx from system mem to video mem.
+
+	libBase -> IGraphics -> WritePixelArray( (uint8 * ) video->Memory, 0, 0, video->BytesPerRow, PIXF_A8R8G8B8, 
+			&video->rp, 0,0, video->width,video->height);
+}
+
