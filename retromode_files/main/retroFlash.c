9e12b0489bf14723aa19766cdaf19b26c1c74d78
Kjetil Hvalstrand <kjetil@teknobingo.no>
2017-10-18 21:31:09 +0200

    Color flash implemented
    Same format as AMOS flash, so if you’re a AMOS programmer you feel at
    home.

--- retromode_files/main/retroFlash.c
+++ retromode_files/main/retroFlash.c
@@ -4,7 +4,7 @@
  *  This file is part of retromode.
  *
  *  Copyright (c) 2017 LiveForIt Software.
- *  MIT License
+ *  MIT License.
  *
  * $Id$
  *
@@ -21,6 +21,8 @@
 #include <libraries/retromode.h>
 #include <proto/retromode.h>
 #include <stdarg.h>
+
+#include "libbase.h"
 
 /****** retromode/main/retroFlash ******************************************
 *
@@ -53,11 +55,111 @@
 *
 */
 
+char *next_char(char *ptr, char c)
+{
+	while (*ptr != 0)
+	{
+		if (*ptr == c) 
+		{
+			ptr++;
+			break;
+		}
+		ptr++;
+	}
+
+	while (*ptr == ' ') ptr++;
+	return ptr;
+}
+
+int get_int(char *ptr,char **to_ptr)
+{
+	int ret = 0;
+
+	while (*ptr == ' ') ptr++;
+
+	while ( (*ptr >='0') && (*ptr<='9') )
+	{
+		ret *= 10;
+		ret += *ptr - '0';
+		ptr++;
+	}
+
+	while (*ptr == ' ') ptr++;
+
+	*to_ptr = ptr;
+
+	return ret;
+}
+
+
+int hex_to_int( char *ptr, char **to_ptr)
+{
+	unsigned char c = *ptr;
+	int ret =0;
+
+	if (ret<15) ret = 0;
+
+	if ((c>='0')&&(c<='9'))	ret = c - '0';
+	if ((c>='a')&&(c<='f'))	ret = c - 'a' + 10;
+	if ((c>='A')&&(c<='F'))	ret = c - 'A' + 10;
+
+	// next char meybe
+	if (*ptr != 0) ptr++;
+	*to_ptr = ptr;
+
+	return ret * 0x11;
+}
+
 struct retroFlashTable * _retromode_retroFlash(struct RetroModeIFace *Self,
        struct retroScreen * screen,
        unsigned char color,
        char * data)
 {
-  return NULL;
+	struct RetroLibrary *libBase = (struct RetroLibrary *) Self -> Data.LibBase;
+	struct retroFlashTable *table;
+
+	int count = 0;
+	char *c;
+	char *sptr;
+
+	for (c=data; *c!=0; c++)
+	{
+		if (*c=='(') count ++;
+	}	
+
+	table = (struct retroFlashTable *) libBase -> IExec -> AllocVecTags( sizeof(struct retroFlashTable),  
+					AVT_Type, MEMF_SHARED, AVT_ClearWithValue, 0 ,TAG_END	);
+
+	if (table)
+	{
+		struct retroFlash *ptr = NULL;
+
+		table -> color = color;
+		table -> colors = count;
+		table -> index = 0;
+		table -> table = (struct retroFlash *) libBase -> IExec -> AllocVecTags( sizeof(struct retroFlash) * count,  
+					AVT_Type, MEMF_SHARED, 
+					AVT_ClearWithValue, 0 ,
+					TAG_END	);
+
+		sptr = data;
+		for (ptr = table -> table; ptr < table -> table+count; ptr++)
+		{
+			sptr = next_char( sptr, '(' );
+			ptr -> rgb.r =	hex_to_int(sptr, &sptr) ;
+			ptr -> rgb.g =	hex_to_int(sptr, &sptr) ;
+			ptr -> rgb.b =	hex_to_int(sptr, &sptr) ;
+
+			libBase -> IDOS -> Printf("RGB %lx,%lx,%lx\n", 
+					(int) ptr->rgb.r,
+					(int) ptr->rgb.g,
+					(int) ptr->rgb.b);
+
+			sptr = next_char( sptr, ',' );
+			ptr -> delay = get_int(sptr,&sptr);
+		}
+	}
+
+	return table;
 }
 
