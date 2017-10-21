25d4725a5e0bc24f529d2f56a50c82a7eaff8203
Kjetil Hvalstrand <kjetil@teknobingo.no>
2017-10-21 15:06:46 +0200

    Delete flash new code.
    Delete flash new code.

---
+++ retromode_files/main/retroDeleteFlash.c
@@ -0,0 +1,61 @@
+/* :ts=4
+ *  $VER: retroDeleteFlash.c $Revision$ (21-Oct-2017)
+ *
+ *  This file is part of retromode.
+ *
+ *  Copyright (c) 2017 LiveForIt Software.
+ *  MIT License
+ *
+ * $Id$
+ *
+ * $Log$
+ *
+ *
+ */
+
+
+#include <exec/exec.h>
+#include <proto/exec.h>
+#include <dos/dos.h>
+#include <exec/types.h>
+#include <libraries/retromode.h>
+#include <proto/retromode.h>
+#include <stdarg.h>
+
+/****** retromode/main/retroDeleteFlash ******************************************
+*
+*   NAME
+*      retroDeleteFlash -- Description
+*
+*   SYNOPSIS
+*      void retroDeleteFlash(struct retroScreen * screen, 
+*          unsigned char color);
+*
+*   FUNCTION
+*
+*   INPUTS
+*       screen - 
+*       color - 
+*
+*   RESULT
+*       This function does not return a result
+*
+*   EXAMPLE
+*
+*   NOTES
+*
+*   BUGS
+*
+*   SEE ALSO
+*
+*****************************************************************************
+*
+*/
+
+void _retromode_retroDeleteFlash(struct RetroModeIFace *Self,
+       struct retroScreen * screen,
+       unsigned char color)
+{
+
+}
+
