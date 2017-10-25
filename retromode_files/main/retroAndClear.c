c9d78031bf27bf9076e6fecfe8fe356b7f5c744b
Kjetil Hvalstrand <kjetil@teknobingo.no>
2017-10-25 21:27:36 +0200

    retroAndClear slightly optimized
    retroAndClear slightly optimized, removing the math from inner loop,
    only needing pointer instead of integer.

--- retromode_files/main/retroAndClear.c
+++ retromode_files/main/retroAndClear.c
@@ -68,19 +68,29 @@ void _retromode_retroAndClear(struct RetroModeIFace *Self,
        int y1,
        unsigned char and_mask)
 {
-	int x,y;
+	int y;
+	unsigned char *xmem,
+		*xmem_start,
+		*xmem_end,
+		*ymem;
 
 	if (x0<0) x0 = 0;
 	if (y0<0) y0 = 0;
-	if (x1>screen->width-1) x1 = screen->width -1;
-	if (y1>screen->height-1) y1 = screen->height-1;
+	if (x1>screen->realWidth-1) x1 = screen->realWidth -1;
+	if (y1>screen->realHeight-1) y1 = screen->realHeight-1;
+
+	ymem = screen -> Memory + (screen -> realWidth * y0);
+	xmem_start = ymem + x0;
+	xmem_end = ymem + x1;
 
 	for(y=y0;y<=y1;y++)
 	{
-		for(x=x0;x<=x1;x++)
+		for(xmem=xmem_start;xmem<=xmem_end;xmem++)
 		{
-			screen -> Memory[ (screen -> width * y) + x ] &= and_mask;
+			*xmem &= and_mask;
 		}
+		xmem_start += screen -> realWidth;
+		xmem_end += screen -> realWidth;
 	}
 }
 
