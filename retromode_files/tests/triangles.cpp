268a1edc099e4d8853ab12e78b5c46368b940ead
Kjetil Hvalstrand <kjetil@teknobingo.no>
2017-10-30 19:23:58 +0100

    Testing retroOrScreenBlit
    Testing retroOrScreenBlit

--- retromode_files/tests/triangles.cpp
+++ retromode_files/tests/triangles.cpp
@@ -480,7 +480,9 @@ int main()
 				for (x=0;x<320;x++)
 				{
 					y = sin(p)*10.0f+20.0f;
-					retroOrBitmapBlit( scroll_rp.BitMap, x,0,1,30, screen, x , y);
+
+					retroOrScreenBlit( screen, x,0,1,30, screen, x , y);
+//					retroOrBitmapBlit( scroll_rp.BitMap, x,0,1,30, screen, x , y);
 					p+=0.05f;
 				}
 			 }
