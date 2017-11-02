8f445471e2244704ef972d5b6d0c511e216916fc
Kjetil Hvalstrand <kjetil@teknobingo.no>
2017-11-02 20:12:05 +0100

    Updated make files and some .h files.
    Updated make files and some .h files.

--- retromode_vectors.c
+++ retromode_vectors.c
@@ -62,6 +62,9 @@ STATIC CONST APTR main_v1_vectors[] =
     _retromode_retroOrStar,
     _retromode_retroPolyLine,
     _retromode_retroPolyGon,
+    _retromode_retroLoadABKSprite,
+    _retromode_retroPasteSprite,
+    _retromode_retroBoing,
     (APTR)-1
 };
 
