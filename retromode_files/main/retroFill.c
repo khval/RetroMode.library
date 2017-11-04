f264f800470c5e13cb78b56514038ba50535f327
Kjetil Hvalstrand <kjetil@teknobingo.no>
2017-11-04 16:24:50 +0100

    New fill functions.
    New fill functions.

---
+++ retromode_files/main/retroFill.c
@@ -0,0 +1,131 @@
+/* :ts=4
+ *  $VER: retroFill.c $Revision$ (04-Nov-2017)
+ *
+ *  This file is part of retromode.
+ *
+ *  Copyright (c) 2017 LiveForIt Software.
+ *  MIT License.
+ *
+ * $Id$
+ *
+ * $Log$
+ *
+ *
+ */
+
+
+#include <exec/exec.h>
+#include <proto/exec.h>
+#include <dos/dos.h>
+#include <exec/types.h>
+#include <libraries/retromode.h>
+#include <proto/retromode.h>
+#include <stdarg.h>
+#include <libbase.h>
+
+/****** retromode/main/retroFill ******************************************
+*
+*   NAME
+*      retroFill -- Description
+*
+*   SYNOPSIS
+*      void retroFill(struct retroScreen * screen, int x, int y, 
+*          unsigned char color);
+*
+*   FUNCTION
+*
+*   INPUTS
+*       screen - 
+*       x - 
+*       y - 
+*       color - 
+*
+*   RESULT
+*       This function does not return a result
+*
+*   EXAMPLE
+*
+*   NOTES
+*
+*   BUGS
+*
+*   SEE ALSO
+*
+*****************************************************************************
+*
+*/
+
+// nice on the stack on recursive call back
+
+struct FillNode
+{
+	struct Node node;
+	int x;
+	int y;
+};
+
+BOOL insideScreenAndReplaceColor(struct retroScreen *screen,int x,int y, char replace_color)
+{
+	if ((x>-1)&&(x<screen->realWidth)&&(y>-1)&&(y<screen->realHeight))
+	{
+		if (screen -> Memory[ screen -> realWidth * y + x ] == replace_color) return TRUE;
+	}
+	return FALSE;
+}
+
+void AddXY( struct RetroLibrary *libBase, struct List *list, int x, int y )
+{
+	struct FillNode *newNode = NULL;
+	newNode = (struct FillNode *) libBase -> IExec-> AllocSysObjectTags( ASOT_NODE, 
+			ASO_NoTrack, TRUE, 
+			ASONODE_Size, sizeof(struct FillNode), TAG_END);
+
+	newNode -> x = x;
+	newNode -> y = y;
+
+	libBase -> IExec-> AddHead( list, (struct Node *) newNode );
+}
+
+void _retromode_retroFill(struct RetroModeIFace *Self,
+       struct retroScreen * screen,
+       int x,
+       int y,
+       unsigned char color)
+{
+	struct RetroLibrary *libBase = (struct RetroLibrary *) Self -> Data.LibBase;
+	char replace_color;
+	struct List list;
+	struct FillNode *node = NULL;
+
+	libBase -> IExec-> NewList(&list);
+
+	if ((x>-1)&&(x<screen->realWidth)&&(y>-1)&&(y<screen->realHeight))
+	{
+		replace_color = screen -> Memory[ screen -> realWidth * y + x ];
+		if (replace_color == color) return;
+	}
+	else return;
+	
+	do
+	{
+		if ( (void *) node == (void *) &list) break;
+
+		if (node)
+		{
+			x = node -> x;
+			y = node -> y;
+			libBase -> IExec-> Remove( (struct Node *) node);
+			libBase -> IExec-> FreeSysObject( ASOT_NODE, node );
+			node = NULL;
+		}
+
+		screen -> Memory[ (screen -> realWidth * y) + x ] = color;
+
+		if (insideScreenAndReplaceColor(screen,x,y-1,replace_color)) AddXY( libBase, &list, x, y-1 );
+		if (insideScreenAndReplaceColor(screen,x,y+1,replace_color)) AddXY( libBase, &list, x, y+1 );
+		if (insideScreenAndReplaceColor(screen,x-1,y,replace_color)) AddXY( libBase, &list, x-1, y );
+		if (insideScreenAndReplaceColor(screen,x+1,y,replace_color)) AddXY( libBase, &list, x+1, y );
+
+	} while (node = (struct FillNode *) libBase -> IExec-> GetHead( &list));
+}
+
---
+++ retromode_files/main/retroOrFill.c
@@ -0,0 +1,116 @@
+/* :ts=4
+ *  $VER: retroOrFill.c $Revision$ (04-Nov-2017)
+ *
+ *  This file is part of retromode.
+ *
+ *  Copyright (c) 2017 LiveForIt Software.
+ *  MIT License.
+ *
+ * $Id$
+ *
+ * $Log$
+ *
+ *
+ */
+
+
+#include <exec/exec.h>
+#include <proto/exec.h>
+#include <dos/dos.h>
+#include <exec/types.h>
+#include <libraries/retromode.h>
+#include <proto/retromode.h>
+#include <stdarg.h>
+#include <libbase.h>
+
+/****** retromode/main/retroOrFill ******************************************
+*
+*   NAME
+*      retroOrFill -- Description
+*
+*   SYNOPSIS
+*      void retroOrFill(struct retroScreen * screen, int x, int y, 
+*          int and_mask, unsigned char or_color);
+*
+*   FUNCTION
+*
+*   INPUTS
+*       screen - 
+*       x - 
+*       y - 
+*       and_mask - 
+*       or_color - 
+*
+*   RESULT
+*       This function does not return a result
+*
+*   EXAMPLE
+*
+*   NOTES
+*
+*   BUGS
+*
+*   SEE ALSO
+*
+*****************************************************************************
+*
+*/
+
+// nice on the stack on recursive call back
+
+struct FillNode
+{
+	struct Node node;
+	int x;
+	int y;
+};
+
+extern BOOL insideScreenAndReplaceColor(struct retroScreen *screen,int x,int y, char replace_color);
+extern void AddXY( struct RetroLibrary *libBase, struct List *list, int x, int y );
+
+void _retromode_retroOrFill(struct RetroModeIFace *Self,
+       struct retroScreen * screen,
+       int x,
+       int y,
+       int and_mask,
+       unsigned char or_color)
+{
+	struct RetroLibrary *libBase = (struct RetroLibrary *) Self -> Data.LibBase;
+	char replace_color;
+	struct List list;
+	struct FillNode *node = NULL;
+
+	libBase -> IExec-> NewList(&list);
+
+	and_mask |= or_color;	// prevents it from go in loop forever.
+
+	if ((x>-1)&&(x<screen->realWidth)&&(y>-1)&&(y<screen->realHeight))
+	{
+		replace_color = screen -> Memory[ screen -> realWidth * y + x ]  & and_mask;
+		if (replace_color == or_color) return;
+	}
+	else return;
+	
+	do
+	{
+		if ( (void *) node == (void *) &list) break;
+
+		if (node)
+		{
+			x = node -> x;
+			y = node -> y;
+			libBase -> IExec-> Remove( (struct Node *) node);
+			libBase -> IExec-> FreeSysObject( ASOT_NODE, node );
+			node = NULL;
+		}
+
+		screen -> Memory[ (screen -> realWidth * y) + x ] |= or_color;
+
+		if (insideScreenAndReplaceColor(screen,x,y-1,replace_color)) AddXY( libBase, &list, x, y-1 );
+		if (insideScreenAndReplaceColor(screen,x,y+1,replace_color)) AddXY( libBase, &list, x, y+1 );
+		if (insideScreenAndReplaceColor(screen,x-1,y,replace_color)) AddXY( libBase, &list, x-1, y );
+		if (insideScreenAndReplaceColor(screen,x+1,y,replace_color)) AddXY( libBase, &list, x+1, y );
+
+	} while (node = (struct FillNode *) libBase -> IExec-> GetHead( &list));
+}
+
