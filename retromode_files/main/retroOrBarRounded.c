43d801a1569c31f44342414c6ee1bb4fc746d214
Kjetil Hvalstrand <kjetil@teknobingo.no>
2017-11-08 17:37:05 +0100

    OrBarRounded was not implemented, now finally it works too.
    OrBarRounded was not implemented, now finally it works too.

--- retromode_files/main/retroOrBarRounded.c
+++ retromode_files/main/retroOrBarRounded.c
@@ -57,6 +57,33 @@
 *
 */
 
+
+void Orhline( struct retroScreen * screen, int x0, int x1, int y, char color )
+{
+	unsigned char *from;
+	unsigned char *to;
+	unsigned char *ptr;
+
+	// exit if out of height
+
+	if (y<0) return;
+	if (y>screen->realHeight-1) return;
+
+	// limit x0 and x1
+
+	if (x0<0) x0 = 0;
+	if (x1>screen->realWidth-1) x1 = screen->realWidth -1;
+
+	from = screen -> Memory + ( screen -> realWidth * y ) + x0;
+	to = from + (x1-x0) ;
+
+	for(ptr=from;ptr<=to;ptr++)
+	{
+		*ptr |= color;
+	}
+}
+
+
 void _retromode_retroOrBarRounded(struct RetroModeIFace *Self,
        struct retroScreen * screen,
        int x0,
@@ -66,6 +93,26 @@ void _retromode_retroOrBarRounded(struct RetroModeIFace *Self,
        int r,
        unsigned char color)
 {
+	int x, y, r2;
+	if (x1-x0<r*2) return;
+	if (y1-y0<r*2) return;
 
+	_retromode_retroOrBAR(Self, screen,  x0+r,  y0,  x1-r,  y0+r, color);
+	_retromode_retroOrBAR(Self, screen,  x0,  y0 + r +1, x1, y1 - r -1, color);
+	_retromode_retroOrBAR(Self, screen,  x0 + r,  y1-r , x1-r, y1 , color);
+
+	r2 = r * r;
+
+	for (y=0;y<r;y++)
+	{
+		x = sqrt( r2 - (y*y));
+		if (x>r) x=r;
+
+		Orhline( screen, x0+r-x, x0+r-1, y0+r-y, color );
+		Orhline( screen, x1-r+1, x1-r+x, y0+r-y, color );
+
+		Orhline( screen, x0+r-x, x0+r-1, y1-r+y, color );
+		Orhline( screen, x1-r+1, x1-r+x, y1-r+y, color );
+	}
 }
 
