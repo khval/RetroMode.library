7046d9f66a1428a22997c64418dfb28b2ac5103a
Kjetil Hvalstrand <kjetil@teknobingo.no>
2017-11-12 12:59:41 +0100

    retroFreeSpriteObjects
    retroFreeSpriteObjects, (Should be called automatic when video output is
    freed.)

---
+++ retromode_files/main/retroFreeSpriteObjects.c
@@ -0,0 +1,67 @@
+/* :ts=4
+ *  $VER: retroFreeSpriteObjects.c $Revision$ (11-Nov-2017)
+ *
+ *  This file is part of retromode.
+ *
+ *  Copyright (c) 2017 LiveForIt Software.
+ *  MIT License.
+ *
+ * $Id$
+ *
+ * $Log$
+ *
+ *
+ */
+
+
+#include <exec/exec.h>
+#include <proto/exec.h>
+#include <dos/dos.h>
+#include <exec/types.h>
+#include <libraries/retromode.h>
+#include <proto/retromode.h>
+#include <stdarg.h>
+#include <libbase.h>
+
+/****** retromode/main/retroFreeSpriteObjects ******************************************
+*
+*   NAME
+*      retroFreeSpriteObjects -- Description
+*
+*   SYNOPSIS
+*      void retroFreeSpriteObjects(struct retroVideo * video);
+*
+*   FUNCTION
+*
+*   INPUTS
+*       video - 
+*
+*   RESULT
+*       This function does not return a result
+*
+*   EXAMPLE
+*
+*   NOTES
+*
+*   BUGS
+*
+*   SEE ALSO
+*
+*****************************************************************************
+*
+*/
+
+void _retromode_retroFreeSpriteObjects(struct RetroModeIFace *Self,
+       struct retroVideo * video)
+{
+	struct RetroLibrary *libBase = (struct RetroLibrary *) Self -> Data.LibBase;
+
+	if (video -> sprites)
+	{
+		libBase->IExec->FreeVec(video -> sprites);
+		video -> sprites = NULL;
+		video -> sprites_end = NULL;
+		video -> spriteObjectsAllocated = 0;
+	}
+}
+
