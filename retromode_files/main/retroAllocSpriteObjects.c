555ba1b6edb55f4f3cfe4ab1e55332539387b495
Kjetil Hvalstrand <kjetil@teknobingo.no>
2017-11-12 12:54:40 +0100

    retroAllocSpriteObjects,
    retroAllocSpriteObjects, retroMode as soft sprites, so there is no limit
    number of sprites, you allocated number of sprites you need, normally
    when Video output freed, SpriteObjects are freed. (Please note sprite
    images are not freed automatic )

---
+++ retromode_files/main/retroAllocSpriteObjects.c
@@ -0,0 +1,73 @@
+/* :ts=4
+ *  $VER: retroAllocSpriteObjects.c $Revision$ (11-Nov-2017)
+ *
+ *  This file is part of retromode.
+ *
+ *  Copyright (c) 2017 LiveForIt Software.
+ *  MIT License.
+ *
+ * $Id$
+ *
+ * $Log$
+ *
+ *
+ */
+
+
+#include <exec/exec.h>
+#include <proto/exec.h>
+#include <dos/dos.h>
+#include <exec/types.h>
+#include <libraries/retromode.h>
+#include <proto/retromode.h>
+#include <stdarg.h>
+#include <libbase.h>
+
+/****** retromode/main/retroAllocSpriteObjects ******************************************
+*
+*   NAME
+*      retroAllocSpriteObjects -- Description
+*
+*   SYNOPSIS
+*      void retroAllocSpriteObjects(struct retroVideo * video, 
+*          int numberOfSprites);
+*
+*   FUNCTION
+*
+*   INPUTS
+*       video - 
+*       numberOfSprites - 
+*
+*   RESULT
+*       This function does not return a result
+*
+*   EXAMPLE
+*
+*   NOTES
+*
+*   BUGS
+*
+*   SEE ALSO
+*
+*****************************************************************************
+*
+*/
+
+void _retromode_retroAllocSpriteObjects(struct RetroModeIFace *Self,
+       struct retroVideo * video,
+       int numberOfSprites)
+{
+	struct RetroLibrary *libBase = (struct RetroLibrary *) Self -> Data.LibBase;
+
+	Self -> retroFreeSpriteObjects( video );
+
+	video -> sprites = (struct retroSpriteObject *) libBase -> IExec-> AllocVecTags( sizeof(struct retroSpriteObject) * numberOfSprites, 
+						AVT_Type, MEMF_SHARED, AVT_ClearWithValue, 0 , TAG_END );
+
+	if (video -> sprites)
+	{
+		video -> sprites_end = video -> sprites + numberOfSprites;
+		video -> spriteObjectsAllocated = numberOfSprites;
+	}
+}
+
