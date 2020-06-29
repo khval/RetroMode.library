09b3d1544df9d951a63033b7a33cee99d253e5bf
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2020-06-29 17:50:04 +0200

    Rewritten retroScroll to be safe to debug, and optimized (moved stuff out of inner loop)

--- retromode_files/main/retroScroll.c
+++ retromode_files/main/retroScroll.c
@@ -13,14 +13,17 @@
  *
  */
 
+#define __USE_INLINE__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <exec/exec.h>
-#include <proto/exec.h>
-#include <dos/dos.h>
 #include <exec/types.h>
-#include <libraries/retromode.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
 #include <proto/retromode.h>
-#include <stdarg.h>
+#include <libraries/retromode.h>
 
 /****** retromode/main/retroScroll ******************************************
 *
@@ -58,6 +61,36 @@
 *
 */
 
+#if 0
+	#define set(adr,v) *(adr)=v
+	#define get(adr) *(adr)
+#else
+
+	#define set(adr,v) if ((( (char *) adr)>=(set_min)) && (( (char *) adr)<=(set_max))) { *(adr)=(v); } else { Printf("set out of range, min %08lx value %08lx max %08lx - ypos %ld\n",set_min,adr,set_max, y); }
+	#define get(adr) __get( ((( (char *) adr)>=(get_min)) &&(( (char *) adr)<=(get_max))) , adr, screen, y )
+
+	#define set_min ((char *) mem)
+	#define set_max ((char *) mem + (screen -> bytesPerRow * screen -> realHeight))
+	#define get_min ((char *) mem)
+	#define get_max ((char *) mem + (screen -> bytesPerRow * screen -> realHeight))
+	#define get_error_code 0
+
+	unsigned char __get( bool in_limit, unsigned char *mem, struct retroScreen *screen, int y )
+	{
+		if (in_limit)
+		{
+			return *mem;
+		}
+		else
+		{
+			Printf("Get out of range, min %08lx value %08lx max %08lx - ypos %ld\n",get_min,mem,get_max, y);
+		}
+	}
+
+#endif
+
+extern struct Interface			*INewlib;
+
 void _retromode_retroScroll(struct RetroModeIFace *Self,
        struct retroScreen * screen,
        int buffer,
@@ -72,43 +105,100 @@ void _retromode_retroScroll(struct RetroModeIFace *Self,
 	unsigned char *mem = screen -> Memory[ buffer ];
 	unsigned char *src;
 	unsigned char *des;
+	unsigned char *row_src;
+	unsigned char *row_dst;
+
 	int x,y;
+
+	Printf("************ %0l8x\n", INewlib);
 
 	if (dy>0)
 	{
+		y1 -= dy;
+
 		if (dx>0)
 		{
+			x1 -=dx;
+
+			printf("%s:%d retro %d,%d to %d,%d\n",__FUNCTION__,__LINE__,x0,y0,x1,y1);
+
+			row_src = mem + x1 + (bytesPerRow * (y1+dy)) ;	
+			row_dst = mem + x1 + (bytesPerRow * y1) +dx ;
+
 			for (y=y1;y>=y0;y--)
 			{
-				src = mem + (bytesPerRow * y) + x1;	des = mem + (bytesPerRow * (y+dy)) + x1 +dx;
-				for (x=x1;x>=x0;x--) *des--=*src--;		
+				src = row_src;	des = row_dst;
+				for (x=x1;x>=x0;x--)
+				{
+					set(des,get(src));
+					des--; src--;
+				}
+				row_src-=bytesPerRow;
+				row_dst-=bytesPerRow;
 			}
 		}
 		else
 		{
+			x0 += -dx;
+
+			printf("%s:%d retro %d,%d to %d,%d\n",__FUNCTION__,__LINE__,x0,y0,x1,y1);
+
+			row_src = mem + x0 + (bytesPerRow * (y1+dy)) ;	
+			row_dst = mem + x0 + (bytesPerRow * y1) +dx ;
+
 			for (y=y1;y>=y0;y--)
 			{
-				src = mem + (bytesPerRow * y) + x0; 	des = mem + (bytesPerRow * (y+dy)) + x0 +dx;
-				for (x=x0;x<=x1;x++) *des++=*src++;		
+				src = row_src;	des = row_dst;
+				for (x=x0;x<=x1;x++)
+				{
+					set(des,get(src));
+					des++; src++;
+				}
+				row_src-=bytesPerRow;
+				row_dst-=bytesPerRow;
 			}
 		}
 	}
 	else
 	{
+		y0 += -dy;
+
 		if (dx>0)
 		{
+			x1 -=dx;
+
+			row_src = mem + x1 + (bytesPerRow * (y0-dy)) ;	// dy is negative number... (-  and - is +)
+			row_dst = mem + x1 + (bytesPerRow * y0) +dx ;
+
 			for (y=y0;y<=y1;y++)
 			{
-				src = mem + (bytesPerRow * (y-dy)) + x1;	des = mem + (bytesPerRow * y) + x1 +dx;
-				for (x=x1;x>=x0;x--) *des--=*src--;		
+				src = row_src;	des = row_dst;
+				for (x=x1;x>=x0;x--)
+				{
+					set(des,get(src));
+					des--; src--;
+				}
+				row_src+=bytesPerRow;
+				row_dst+=bytesPerRow;
 			}
 		}
 		else
 		{
+			x0 += -dx;								// dx is negative number... (-  and - is +)
+
+			row_src = mem + x0 + (bytesPerRow * (y0-dy)) ;	// dy is negative number... (-  and - is +)
+			row_dst = mem + x0 + (bytesPerRow * y0) +dx ;
+
 			for (y=y0;y<=y1;y++)
 			{
-				src = mem + (bytesPerRow * (y-dy)) + x0; 	des = mem + (bytesPerRow * y) + x0 +dx;
-				for (x=x0;x<=x1;x++) *des++=*src++;		
+				src = row_src;	des = row_dst;
+				for (x=x0;x<=x1;x++)
+				{
+					set(des,get(src));
+					des++; src++;
+				}
+				row_src+=bytesPerRow;
+				row_dst+=bytesPerRow;
 			}
 		}
 	}
